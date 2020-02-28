0036df43dd10c84272f26cffacbc5dd1716cc624
Justin Montgomery <montgomerycodepro@gmail.com>
2020-02-27 23:10:06 -0500

    got all the employee tests passing

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,18 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+class Engineer extends Employee {
+    constructor(name, id, email, gitHub){
+        super(name, id ,email);
+
+        this.gitHubAccount = gitHub;
+    }
+    getGitHub(){
+        return this.gitHubAccount;
+    }
+}
+
+
+
+
+
+module.exports = Engineer
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
+const Manager = require("./Develop/lib/Manager");
+const Engineer = require("./Develop/lib/Engineer");
+const Intern = require("./Develop/lib/Intern");
+const Employee = require("./Develop/lib/Employee")
 const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
@@ -8,7 +9,11 @@ const fs = require('fs');
 const OUTPUT_DIR = path.resolve(__dirname, 'output');
 const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
-const render = require('./lib/htmlRenderer');
+const render = require('./Develop/lib/htmlRenderer');
+
+const engineer1 = new Engineer("Justin", 1, "test@email.com", "JustinMonty20");
+// console.log(engineer1.gitHub)
+console.log(engineer1)
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
