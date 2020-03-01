df8c82f0b97db590b2ccbaf13096a2772961e3fa
Justin Montgomery <montgomerycodepro@gmail.com>
2020-03-01 12:59:54 -0500

    creating the folder if it does not exist

--- app.js
+++ app.js
@@ -5,34 +5,24 @@ const Intern = require("./Develop/lib/Intern");
 const Employee = require("./Develop/lib/Employee");
 const inquirer = require('inquirer');
 const path = require('path');
-const fs = require('fs');
-let team = [];
-
+const util = require("util")
+const fs = require('fs-extra');
 const OUTPUT_DIR = path.resolve(__dirname, 'output');
 const outputPath = path.join(OUTPUT_DIR, 'team.html');
-
+const fsWriteFileAsync = util.promisify(fs.writeFile);
 const render = require('./Develop/lib/htmlRenderer');
 
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
-
+let team = [];
+// async function that will populate "team" with objects based on the user responses.
 async function init (){
     let teamBuilding;
+    // do - while loop. 
+    // user will continue to be asked the questions while their answer to the team building question is "yes".
     do {
         try { response = await inquirer.prompt(questions.employee)
                 let specific;
+            // switch case that checks for the the answer to response.role 
+            // whatever their answer to that question is will ask the user 
             switch(response.role) {
                 case "Engineer":
                 specific = await inquirer.prompt(questions.engineer) 
@@ -62,28 +52,17 @@ async function init (){
         }
 
         teamBuilding = await inquirer.prompt(questions.finished)
-    
+       
     } while (teamBuilding.build === "Yes".toLocaleLowerCase())
+    
+
+    fs.ensureDir(OUTPUT_DIR).then(()=>{
+        fsWriteFileAsync(outputPath, render(team));
+    })
     
  }
 
 init();
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above to target this location.
-
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not. The fs npm package may have methods to check if a directory exists, and they
-// may also have methods to create a directory that doesn't...
-
-
-
-
-
 
 // first attempt at the switch statement
 // const employee = await inquirer.prompt(questions.role)
